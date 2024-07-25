4581b298fb1d9338517f10985e59c646f224806b
Chaitanya Soni <sonichaitanya13@gmail.com>
2024-07-25 12:42:32 +0530

    outlets and actions

--- GUI Calculator/GUI Calculator/ViewController.swift
+++ GUI Calculator/GUI Calculator/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = ""
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        print(#function)
+        label.text = "3"
+    }
+    
 }
 

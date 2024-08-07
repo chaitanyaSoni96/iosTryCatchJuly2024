d34deea0ca5c621188b8d3d7d63f88db964161dc
your_username <sonichaitanya13@gmail.com>
2024-08-07 13:37:02 +0530

    ui basics

--- UI Basics/UI Basics/ViewController.swift
+++ UI Basics/UI Basics/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorWell: UIColorWell!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "app bg")
+        imageView.contentMode = .scaleAspectFill
+        
+        self.view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+        colorWell.addTarget(self, action: #selector(colorWellChanged(_:)), for: .valueChanged)
+        
+        button.setTitle("Some", for: UIControl.State.normal)
+        
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let safeAreaInsets = self.view.safeAreaInsets
+        print(safeAreaInsets)
+    }
+    
+    
+    @objc func colorWellChanged(_ sender: UIColorWell) {
+        self.view.backgroundColor = sender.selectedColor
+    }
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
 }
 

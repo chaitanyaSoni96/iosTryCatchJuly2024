0339cc60a4ff600d8bf4dfd8cc71d97203607110
your_username <sonichaitanya13@gmail.com>
2024-10-21 13:10:34 +0530

    Working with audio and video

--- Working with Audio/Working with AV/AppDelegate.swift
+++ Working with Audio/Working with AV/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        try? AVAudioSession.sharedInstance().setActive(true, options: [])
+        
         return true
     }
 

95f1d085c0c73f0d4702416a6407a153c02967f7
your_username <sonichaitanya13@gmail.com>
2025-03-19 13:29:49 +0530

    implemented dropdown

--- ThirdPartyLibrary example/ThirdPartyLibrary example/ViewController.swift
+++ ThirdPartyLibrary example/ThirdPartyLibrary example/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import DropDown
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+        let dropdown = DropDown()
+        dropdown.anchorView = sender
+        dropdown.dataSource = ["Jan", "Feb", "March", "April"]
+        dropdown.selectionAction = { index, title in
+            print(index, title)
+            self.label.text = title
+        }
+        dropdown.direction = .bottom
+        dropdown.bottomOffset = .init(x: 0, y: sender.frame.height)
+        dropdown.show()
+    }
 }
 

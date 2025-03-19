bc5f660f6cee5619694e273679bfebe505ec1804
your_username <sonichaitanya13@gmail.com>
2025-03-19 13:11:04 +0530

    3p lib project setup

---
+++ ThirdPartyLibrary example/ThirdPartyLibrary example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdPartyLibrary example
+//
+//  Created by Chaitanya Soni on 19/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
